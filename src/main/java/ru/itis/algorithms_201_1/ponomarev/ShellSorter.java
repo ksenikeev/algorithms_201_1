a27d5115988725381ca0db127c17e9f6170255de
Ilya Ponomarev <iponom0420@gmail.com>
2023-03-06 19:52:49 +0300

    create class

---
+++ src/main/java/ru/itis/algorithms_201_1/ponomarev/ShellSorter.java
@@ -0,0 +1,11 @@
+package ru.itis.algorithms_201_1.ponomarev;
+
+public class ShellSorter {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+    public static void sort(int[] arr) {
+
+    }
+}
