285c49abea4d745937bae74c550318d3a8287899
lldan <safinildan@gmail.com>
2023-03-22 19:08:42 +0300

    add test

--- src/main/java/ru/itis/algorithms_201_1/safin/Main.java
+++ src/main/java/ru/itis/algorithms_201_1/safin/Main.java
@@ -2,15 +2,8 @@ package ru.itis.algorithms_201_1.safin;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[1000];
-        for (int i = 0; i < 1000; i++) {
-            int a = (int) ( Math.random() * 30000 );
-            arr[i] = a;
-        }
-        System.out.println(MergeSort.sort(arr, arr.length));
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
+        TestAlgorithm.createTest();
+        TestAlgorithm.writeStatistics();
     }
 
 
