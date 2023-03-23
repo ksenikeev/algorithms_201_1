9e6c69fedfcd20fef664a6945e6345c46a00aa3e
Rostislav Sergeev <113761086+itrostik@users.noreply.github.com>
2023-03-23 17:46:40 +0300

    Delete src/main/java/ru/itis/algorithms_201_1/sergeev/sergeev directory

--- src/main/java/ru/itis/algorithms_201_1/sergeev/sergeev/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.algorithms_201_1.sergeev;
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        TestControl.createTests();
-        TestControl.writeOutput();
-    }
-}
