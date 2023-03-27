1afcb9badf3398444d012074f9183e3657022f91
ksenikeev <31629851+ksenikeev@users.noreply.github.com>
2023-03-27 22:12:30 +0300

    Revert "Boyer moore algorithm"

--- src/main/java/ru/itis/algorithms_201_1/kashshapov/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.algorithms_201_1.kashshapov;
-
-public class Main {
-    public static void main(String[] args) {
-        TestManipulator.createTests();
-        TestManipulator.writeOutput();
-        TestManipulator.printOutput();
-    }
-}
